e60fe18ba69b89dc3a6fbd08a454ae9e15e45093
puzanov <puzanov@gmail.com>
2017-05-21 13:09:24 +0600

    added ctags apt install command and parse dir changed

--- createtags.sh
+++ createtags.sh
@@ -1,8 +1,10 @@
 # */5 * * * * oleg  /home/oleg/createtags.sh
 #!/bin/bash
+sudo apt install exuberant-ctags
 exec ctags-exuberant -f ~/tags \
 -h "*.*" -R \
 --exclude="\.svn" \
+--exclude="\.git" \
 --totals=yes \
 --PHP-kinds=+cf \
-/mnt/src/namba/dev/
+~/src/
